7776eb820d9c17a71c05df4d9c91f7f8e7d667ff
shravan reddy <shravankolla1976@gmail.com>
2023-08-14 11:18:06 +0530

    Assignment Done

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,8 +5,8 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
-        this.addExtraCheese();
-        this.addExtraToppings();
+        super.addExtraCheese();
+        super.addExtraToppings();
 
 
     }
